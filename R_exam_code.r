886df2137063d96a9814fdb71239005067653be9
Leonardo <73693895+leonardozabotti@users.noreply.github.com>
2021-01-22 13:03:25 +0100

    Update R_exam_code.r

--- R_exam_code.r
+++ R_exam_code.r
@@ -30,3 +30,43 @@ cl <- colorRampPalette(c('red','white','grey'))(100)
 
 #Plotting the differences
 plot(dif, col= cl)
+
+
+
+#### TRYING THE SAME THING WITH IMAGES WITH LOWER RESOLUTION (1 km)
+aug18.1 <- raster("c_gls_NDVI_201808010000_GLOBE_PROBAV_V2.2.1.nc")
+aug19.1 <- raster("c_gls_NDVI_201908010000_GLOBE_PROBAV_V2.2.1.nc")
+
+# Cropping images before plotting to avoid high time computer elaboration on heavy images 
+ext <- c(10,15,45,48)
+aug19_NEita.1 <- crop(aug19.1, ext)
+aug18_NEita.1 <- crop(aug18.1, ext)
+
+#First plotting
+plot(aug19_NEita.1)
+plot(aug19_NEita.1)
+
+#Checking for differences in NDVI
+dif.1 <- aug19_NEita.1 - aug18_NEita.1
+
+#Creating a colour palette that highlights negative differences (biomass losses)
+cl <- colorRampPalette(c('red','white','grey'))(100)
+
+#Plotting the differences
+plot(dif.1, col= cl)
+
+# Checking if lower resolution is stil valid 
+par(mfrow=c(1,2))
+plot(dif, col= cl, main="300m Resol.")
+plot(dif.1, col= cl, main= "1km Resol.")
+
+dev.off()
+
+### CONSIDERATION: 300m is better so we'll keep working with this resol. 
+
+###----------------- 
+
+# Let's now check for differences between the week before and after the storm (26-30 Oct. 2018)
+
+
+
